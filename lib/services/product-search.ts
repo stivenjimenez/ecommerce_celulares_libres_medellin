45cb0f69c8c3eb9eba5aaeb112b7c4099ecf9a44
SenkuJimenez <senkujimenez.dev@gmail.com>
2026-04-05 19:14:22 +0000

    feat: move product search to dedicated page

---
+++ lib/services/product-search.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import useSWR from "swr";
+
+import { type Product } from "@/lib/domain/product";
+import { fetcher } from "@/lib/services/fetcher";
+
+export function useProductSearch(query: string) {
+  const params = new URLSearchParams({ q: query });
+  const key = `/api/products/search?${params.toString()}`;
+
+  return useSWR<Product[]>(key, fetcher<Product[]>);
+}
